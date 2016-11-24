5359e39113766497783a9753dfd983258a476663
donovanCarreno <donovan_carreno@yahoo.com>
2016-11-24 16:07:15 -0600

    hard coded SF, SD

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, browserHistory, IndexRoute} from 'react-router'
-import {apiKey} from '../../key'
+import {apiKey, mapsKey} from '../../key'
 
 class App extends React.Component {
   constructor() {
@@ -10,7 +10,8 @@ class App extends React.Component {
       data: {
         default: "stuff"
       },
-      icon: 'http://bit.ly/2fW8bgt'
+      icon: 'http://bit.ly/2fW8bgt',
+      temp: 55
     }
   }
 
@@ -20,22 +21,36 @@ class App extends React.Component {
     })
     .then((data) => {
       let icon = this.state.icon
+      let summary = data.currently.summary
+      let temp = Math.round(data.currently.temperature).toFixed(0)
       // http://bit.ly/2glGXR6 cloudy
       if (data.currently.icon === 'cloudy') {
-        icon = 'http://bit.ly/2glGXR6'
+        icon = 'http://bit.ly/1NlhgeK'
       }
 
       this.setState({
         data,
-        icon
+        icon,
+        summary,
+        temp
       })
     })
+    // this.setState({
+    //   temp: Math.round(this.state.temp).toFixed(0)
+    // })
   }
 
   render() {
     return (
-      <div>
-        <img src={this.state.icon}></img>
+      <div className="container">
+        <header>This is my header</header>
+        <div className="main">
+          <p className="summary">{this.state.summary}</p>
+          <p className="temp">{this.state.temp}&#8457;</p>
+          <p className="city">Sioux Falls, SD</p>
+          <img src={this.state.icon}></img>
+        </div>
+        <footer>This is my footer</footer>
       </div>
     )
   }
