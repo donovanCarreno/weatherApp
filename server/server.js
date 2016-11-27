3ee3fa87987b6858f458699a1406e669844cbd1d
donovanCarreno <donovan_carreno@yahoo.com>
2016-11-27 07:49:14 -0600

    clean up server

--- server/server.js
+++ server/server.js
@@ -3,9 +3,4 @@ const app = express()
 
 app.use(express.static(__dirname + '/../public'))
 
-app.get('/test', function (req, res) {
-  console.log('hola')
-  res.status(200).json({server: "data!"})
-})
-
 module.exports = app
