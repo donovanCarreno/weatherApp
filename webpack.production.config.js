a1e90b7110fa83100ba3be9d5ed134850fc53bcd
donovanCarreno <donovan_carreno@yahoo.com>
2016-11-29 19:43:12 -0600

    configure for production

---
+++ webpack.production.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack')
+
+module.exports = {
+  entry: __dirname + '/src/components/App.jsx',
+
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      comments: false,
+    })
+  ]
+}
