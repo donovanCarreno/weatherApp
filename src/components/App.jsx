a8c6ca3d6e8951a2ddf2402cf48e8f934cfa1b8e
donovanCarreno <donovan_carreno@yahoo.com>
2016-12-01 09:43:30 -0600

    safari does not support fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ class App extends React.Component {
 
   handleSubmit(address) {
       browserHistory.push(`/forecast/${address}`)
-      fetch(`/forecast/${address}`)
+      $.ajax(`/forecast/${address}`)
       .then(res => (
         res.json()
         .then(data => {
